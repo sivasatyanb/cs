74b7d8a9ee61541d11ce7a9b7b571318013bc51f
siva <sivasatyan.b@gmail.com>
2024-09-24 14:57:10 +0100

    fibonacci

---
+++ class/wc_23_sep/fibonacci.py
@@ -0,0 +1,15 @@
+# generating the fibonacci sequence using recursion:
+def fibonacci(n):
+    
+    if (n == 0):
+        return 0
+    elif (n == 1 or n == 2):
+        return 1
+    else:
+        return (fibonacci(n - 1) + fibonacci(n - 2))
+
+# get user input and print function output
+n = int(input('Enter a number of terms from the Fibonacci sequence: '))
+print(f'The Fibonacci sequence for the first {n} terms are:', end = ' ')
+for i in range(n):
+    print(f'{fibonacci(i)}', end = ' ')
